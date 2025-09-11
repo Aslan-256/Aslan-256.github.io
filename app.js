9c559be238e217493f037f8e4a307fe356a35247
Aslan-256 <s341631@studenti.polito.it>
2025-09-12 00:35:09 +0200

    color and bg fixes

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ particlesJS('particles-js',
       }
     },
     "color": {
-      "value": "#ff3232"
+      "value": "#35ff32ff"
     },
     "shape": {
       "type": "circle",
       "stroke": {
         "width": 0,
-        "color": "#ff3232"
+        "color": "#35ff32ff"
       },
       "polygon": {
         "nb_sides": 5
--- particles.js
+++ particles.js
@@ -33,7 +33,7 @@ var pJS = function(tag_id, params){
         type: 'circle',
         stroke: {
           width: 0,
-          color: '#00ff26ff'
+          color: '#35ff32ff'
         },
         polygon: {
           nb_sides: 5
